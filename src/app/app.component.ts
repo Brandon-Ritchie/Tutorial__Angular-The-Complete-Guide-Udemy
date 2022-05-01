c7cdbcc7d9394349ab963d1d31828c6573d9de31
Brandon Ritchie <brandonritchie@Brandons-MacBook-Pro.local>
2022-05-01 11:42:41 -0400

    Learned about css styling in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  // styleUrls: ['./app.component.css'],
+
+  // Can use styles instead on styleUrls if it is short
+  styles: [
+    `
+      h3 {
+        color: dodgerblue;
+      }
+    `,
+  ],
 })
 export class AppComponent {}
